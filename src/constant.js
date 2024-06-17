736cbbdd107286e0d46eddad287ed075f88f6f51
hirana <daush128@gmail.com>
2024-06-17 13:04:36 +0700

    adding env

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,5 @@
-export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
-export const API_KEY = 'e329252c7f7268bcfebc128d5eb91f5a';
+import { config } from "dotenv";
+config();
+
+export const API_KEY = process.env.API_KEY;
+export const BASE_URL = process.env.BASE_URL;
